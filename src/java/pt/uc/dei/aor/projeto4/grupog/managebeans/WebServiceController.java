b229837b9e744f2f16cf8bbac806c4e94c350399
cortezsusana <susana.ccortez@gmail.com>
2014-04-28 15:33:27 +0100

    webservice

---
+++ src/java/pt/uc/dei/aor/projeto4/grupog/managebeans/WebServiceController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pt.uc.dei.aor.projeto4.grupog.managebeans;
+
+import javax.enterprise.context.Dependent;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Bruno Maricato
+ */
+@Named(value = "webServiceController")
+@Dependent
+public class WebServiceController {
+
+    /**
+     * Creates a new instance of WebServiceController
+     */
+    public WebServiceController() {
+    }
+
+    public void checkLyric() {
+
+    }
+
+}
